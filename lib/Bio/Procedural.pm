ae694438ba6ec0c6f5ebeabcece5680f91b12c68
Chris Fields <cjfields@illinois.edu>
2019-11-29 14:19:06 -0600

    add stub module for dist

---
+++ lib/Bio/Procedural.pm
@@ -0,0 +1,26 @@
+package Bio::Procedural;
+
+use utf8;
+use strict;
+use warnings;
+
+1;
+
+# ABSTRACT: Perl modules for biology
+# AUTHOR: See the individual modules for their authors.
+# OWNER: many people (see the individual modules for their copyright holders)
+# LICENSE: Perl_5
+
+=head1 SYNOPSIS
+
+See L<Bio::Perl> for examples.
+
+=head1 DESCRIPTION
+
+The code in this distribution focuses on simple low-dependency procedural
+interfaces to BioPerl that don't require knowledge of BioPerl classes and
+methods.
+
+=cut
+
+__END__
